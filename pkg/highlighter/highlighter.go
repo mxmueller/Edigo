35c8a4a63963c8f482ccd63adf7518b1c92a174d
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-09-20 01:53:43 +0200

    feat: fix color interceptance

--- pkg/highlighter/highlighter.go
+++ pkg/highlighter/highlighter.go
@@ -3,6 +3,8 @@ package highlighter
 import (
 	"regexp"
 	"strings"
+
+	"github.com/charmbracelet/lipgloss"
 )
 
 // TokenType repräsentiert verschiedene Arten von Tokens in der Sprache
@@ -43,7 +45,7 @@ type Rule struct {
 func NewSyntaxDefinition() *SyntaxDefinition {
 	return &SyntaxDefinition{
 		Keywords:    []string{"if", "else", "for", "func", "return", "var", "const"},
-		Operators:   []string{"+", "-", "*", "/", "==", "!=", "<", ">", "<=", ">=", " "},
+		Operators:   []string{"+", "-", "*", "/", "==", "!=", "<", ">", "<=", ">="},
 		Punctuation: []string{"(", ")", "{", "}", "[", "]", ",", ";", "."},
 		Rules: []Rule{
 			{regexp.MustCompile(`\b[0-9]+(\.[0-9]+)?\b`), TokenNumber, 0},
@@ -111,6 +113,66 @@ func (sd *SyntaxDefinition) TokenizeLine(line string) []Token {
 			remaining = remaining[1:]
 		}
 	}
-
 	return tokens
+}
+
+func stripColorCodes(text string) string {
+    // Compile the regular expression for ANSI escape codes
+    re := regexp.MustCompile(`\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])`)
+    
+    // Replace all occurrences of ANSI escape codes with an empty string
+    return re.ReplaceAllString(text, "")
+}
+
+func (sd *SyntaxDefinition) ColorText(text string) string {
+	var result strings.Builder
+
+    cleanText := stripColorCodes(text)
+    tokens := sd.TokenizeLine(cleanText)
+	remainingText := text
+
+	keywordStyle :=     lipgloss.NewStyle().Foreground(lipgloss.Color("#6C48C5"))
+	stringStyle :=      lipgloss.NewStyle().Foreground(lipgloss.Color("#41B3A2"))
+	numberStyle :=      lipgloss.NewStyle().Foreground(lipgloss.Color("#0000FF"))
+	commentStyle :=     lipgloss.NewStyle().Foreground(lipgloss.Color("#FFFF00"))
+	operatorStyle :=    lipgloss.NewStyle().Foreground(lipgloss.Color("#FF00FF"))
+	punctuationStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#00FFFF"))
+
+	for len(remainingText) > 0 {
+		longestMatch := ""
+        var tokenType TokenType
+
+		for _, token := range tokens {
+			if strings.HasPrefix(remainingText, token.Value) {
+                longestMatch = token.Value
+                tokenType = token.Type
+                break
+			}
+		}
+
+		if longestMatch != "" {
+            switch tokenType {
+                case TokenKeyword:
+                    result.WriteString(keywordStyle.Render(longestMatch))
+                case TokenString:
+                    result.WriteString(stringStyle.Render(longestMatch))
+                case TokenNumber:
+                    result.WriteString(numberStyle.Render(longestMatch))
+                case TokenComment:
+                    result.WriteString(commentStyle.Render(longestMatch))
+                case TokenOperator:
+                    result.WriteString(operatorStyle.Render(longestMatch))
+                case TokenPunctuation:
+                    result.WriteString(punctuationStyle.Render(longestMatch))
+                default:
+                    result.WriteString(longestMatch)
+		}
+			remainingText = remainingText[len(longestMatch):]
+		} else {
+			result.WriteByte(remainingText[0])
+			remainingText = remainingText[1:]
+		}
+	}
+
+	return result.String()
 }
